2ff1c09547a3027b277ea037817cd16c81d020dd
ankitjha93 <93ankitjha@gmail.com>
2024-10-20 12:13:17 +0530

    header had added

--- app/dashboard/layout.jsx
+++ app/dashboard/layout.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
+import Header from './_components/Header'
 
 function Dashboardlayout({children}) {
   return (
     <div>
+      <Header/>
       {children}
     </div>
   )
